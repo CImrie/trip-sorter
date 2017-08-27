1cbe1770adc044d86e3209d238daaea89dd21b4b
Connor Imrie <connorimrie@outlook.com>
2017-08-27 22:40:40 +0100

    Implement flight data import from data array.

--- src/BoardingPasses/Flight.php
+++ src/BoardingPasses/Flight.php
@@ -8,4 +8,120 @@ use CImrie\FlightSorter\BoardingPass;
 
 class Flight extends BoardingPass {
 
+	/**
+	 * @var string
+	 */
+	protected $baggageInformation;
+
+	/**
+	 * @var string
+	 */
+	protected $seat;
+
+	/**
+	 * @var string
+	 */
+	protected $gate;
+
+	/**
+	 * @var string
+	 */
+	protected $flightNumber;
+
+	/**
+	 * @return string
+	 */
+	public function getBaggageInformation()
+	{
+		return $this->baggageInformation;
+	}
+
+	/**
+	 * @param string $baggageInformation
+	 *
+	 * @return Flight
+	 */
+	public function setBaggageInformation($baggageInformation)
+	{
+		$this->baggageInformation = $baggageInformation;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getSeat()
+	{
+		return $this->seat;
+	}
+
+	/**
+	 * @param string $seat
+	 *
+	 * @return Flight
+	 */
+	public function setSeat($seat)
+	{
+		$this->seat = $seat;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getGate()
+	{
+		return $this->gate;
+	}
+
+	/**
+	 * @param string $gate
+	 *
+	 * @return Flight
+	 */
+	public function setGate($gate)
+	{
+		$this->gate = $gate;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getFlightNumber()
+	{
+		return $this->flightNumber;
+	}
+
+	/**
+	 * @param string $flightNumber
+	 *
+	 * @return Flight
+	 */
+	public function setFlightNumber($flightNumber)
+	{
+		$this->flightNumber = $flightNumber;
+
+		return $this;
+	}
+
+	/**
+	 * @param array $data
+	 *
+	 * @return $this
+	 */
+	public function fromArray(array $data)
+	{
+		parent::fromArray($data);
+
+		$this->baggageInformation = $data['baggage'];
+		$this->seat = $data['seat'];
+		$this->gate = $data['gate'];
+		$this->flightNumber = $data['flight'];
+
+		return $this;
+	}
 }
---
+++ tests/FlightTest.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace CImrie\FlightSorter\Tests;
+
+
+use CImrie\FlightSorter\BoardingPasses\Flight;
+
+class FlightTest extends TestCase {
+
+	/**
+	 * @test
+	 */
+	public function can_import_flight_from_data_array()
+	{
+		$flight = new Flight();
+		$flight->fromArray(
+			$data = [
+				'baggage' => 'Baggage drop at ticket counter 344',
+				'seat'    => '3A',
+				'gate'    => '45B',
+				'to'      => 'Stockholm',
+				'from'    => 'Gerona Airport',
+				'flight'  => 'SK455',
+			]
+		);
+
+		$this->assertEquals($data['baggage'], $flight->getBaggageInformation());
+		$this->assertEquals($data['seat'], $flight->getSeat());
+		$this->assertEquals($data['gate'], $flight->getGate());
+		$this->assertEquals($data['to'], $flight->getDestination());
+		$this->assertEquals($data['from'], $flight->getSource());
+		$this->assertEquals($data['flight'], $flight->getFlightNumber());
+	}
+}
